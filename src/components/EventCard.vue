499dcf6b2fe7dadb716e3d007b44d89489c56b8f
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2020-04-18 18:14:46 +0200

    real world vue tutorial

---
+++ src/components/EventCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h4>{{title}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: {
+        id: 1,
+        title: 'Beach cleanup',
+        date: 'Tues Aubg 19'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+h4 {
+  color: green;
+}
+</style>
---
+++ src/views/UserShow.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hey {{username}}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+    },
+  },
+}
+</script>
+
+<style>
+</style>
